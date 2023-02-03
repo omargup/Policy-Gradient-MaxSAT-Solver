f7984ff6c2c6974d346b699bf4ed617b464c84ad
Omar Gutiérrez <omar.gup@gmail.com>
2023-02-03 14:02:12 -0600

    add satrand_dataset

--- build_dataset.py
+++ build_dataset.py
@@ -1,24 +1,11 @@
-# Building the dataset for the paper:
-#
-# This script generates the following instances.
-# For k=3 and n=[20, 30, 40]:
-# 5 Uniform random instances with r=2.0
-# 5 Uniform random instances with r=2.5
-# 5 Uniform random instances with r=3.0
-# 5 Uniform random instances with r=3.5
-# 5 Uniform random instances with r=4.0
-# 5 Uniform random instances with r=4.5
-#
-# For n=[20, 30, 40]:
-# 5 SR instances with k = 1 + B(0.7) + G(0.4)
-# ==============================================================================
-
 from src.sat_generator import URGenerator, SRGenerator
+from src.solvers import minisat_solver
+
 import numpy as np
 from tqdm import tqdm
 
 
-def URdataset():
+def rand_dataset():
     """
     This function generates the following instances:
     For k=3 and n=[20, 30, 40]:
@@ -31,7 +18,7 @@ def URdataset():
     """
 
     # Build Uniform random instances
-    dir_name = 'data'
+    dir_name = 'data/rand'
     n_list = [20, 30, 40]
     r_list = [2.0, 2.5, 3.0, 3.5, 4.0, 4.5]
     k = 3
@@ -58,7 +45,52 @@ def URdataset():
                 sat_gen.save(n, formula, filename)
 
 
-def SRdataset():
+def satrand_dataset():
+    """
+    Builds a dataset with Satisfiable Random SAT Formulas.
+    This function generates 5 uniform random sat instances
+    for each of the following combinations of parameters:
+        k=3
+        n=[20, 30, 40]
+        r=[2.0, 2.5, 3.0, 3.5, 4.0, 4.5]:
+    """
+
+    # Build Uniform random instances
+    dir_name = 'data/satrand'
+    n_list = [20, 30, 40]
+    r_list = [2.0, 2.5, 3.0, 3.5, 4.0, 4.5]
+    k = 3
+    num_instances = 5
+
+    np.random.seed(104873)
+
+    for n in tqdm(n_list):
+        for r in r_list:
+            # Instantiate a sat generator
+            sat_gen = URGenerator(min_n=n,
+                                  max_n=n,
+                                  min_k=k,
+                                  max_k=k,
+                                  min_m=int(n * r),
+                                  max_m=int(n * r))
+
+            sat_clauses = 0
+            while sat_clauses < num_instances:
+                # Create a uniform random sat formula
+                n, m, r, formula = sat_gen.generate_formula()
+
+                # Using minisat_solver to check satifiability
+                assignment, is_sat = minisat_solver(n, formula)
+
+                if is_sat:
+                    sat_clauses += 1
+
+                    # Saving the formula
+                    filename = sat_gen.get_filename(dir_name, sat_clauses)
+                    sat_gen.save(n, formula, filename)
+
+
+def sr_dataset():
     """
     This function generates the following instances:
     For n=[20, 30, 40]:
@@ -66,7 +98,7 @@ def SRdataset():
     """
 
     # Build SR random instances
-    dir_name = 'data'
+    dir_name = 'data/sr'
     n_list = [20, 30, 40]
     p_bernoulli = 0.7 
     p_geometric = 0.4
@@ -94,5 +126,6 @@ def SRdataset():
 
 
 if __name__ == '__main__':
-    URdataset()
-    SRdataset()
+    rand_dataset()
+    satrand_dataset()
+    sr_dataset()
