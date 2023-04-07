0ba4f83fd5f03ae0bd68b133586664912957cb08
Omar Gutiérrez <omar.gup@gmail.com>
2023-04-07 16:12:02 -0600

    add some minor changes

--- experiment_v2.py
+++ experiment_v2.py
@@ -18,33 +18,24 @@ import time
 # Experiment v2                                     #
 #####################################################
 
-#####################################################
-# First step:                                       #
-# Build node2vec emb with hyperparameters search    #
-#####################################################
-
-#####################################################
-# Second step:                                      #
-# Run pg_solver hyperparameters search              #
-#####################################################
-
-#####################################################
-# Third step:                                       #
-# Run pg_solver with the best hyperparameters       #
-#####################################################
-
-
 def paths_for_instances(num_vars=20, data_path='data/rand'):
-    # Returs the path for every instance with n variables in the data_path.
+    '''
+    Returs the path for every instance with num_vars variables in the data_path.
+    '''
+    
     data_path = os.path.abspath(os.path.join(data_path, str(f'{num_vars:04d}')))
     paths = []
     for root, dirs, files in os.walk(data_path):
         for filename in files:
             paths.append(os.path.join(root, filename))
+    paths = sorted(paths)
     return paths
 
 
 def node2vec_tune(config):
+    '''
+    Wrapper to handle the node2vec function with raytune.
+    '''
     device = 'cpu'
     if config['gpu']:
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
@@ -72,9 +63,10 @@ def n2v_hypersearch(instance_dir,
                     n2v_num_epochs=100,
                     exp_name='node2vec',
                     raytune_trials=35,
-                    raytune_dir="hypersearch",
+                    raytune_dir='hypersearch',
                     grace_period=5,
-                    scheduler_max_t=50):
+                    scheduler_max_t=50,
+                    resources_per_trail={"cpu": 6, "gpu": 0.5}):
     
     def define_by_run_func(trial):
         trial.suggest_categorical('n2v_batch_size', [8, 16, 32, 64, 128])
@@ -104,7 +96,7 @@ def n2v_hypersearch(instance_dir,
         return config
     
     
-    torch.cuda.empty_cache()
+    #torch.cuda.empty_cache()
     sampler = optuna.samplers.TPESampler(n_startup_trials=10,
                                          n_ei_candidates=24,
                                          multivariate=True)
@@ -139,7 +131,7 @@ def n2v_hypersearch(instance_dir,
                                log_to_file=True)
 
     # We have 1 GPU and 12 cpus, this will run 2 concurrent trials at a time.
-    trainable_with_cpu_gpu = tune.with_resources(node2vec_tune, {"cpu": 6, "gpu": 0.5})
+    trainable_with_cpu_gpu = tune.with_resources(node2vec_tune, resources_per_trail)
     tuner = tune.Tuner(trainable_with_cpu_gpu,
                         tune_config=tune_config,
                         run_config=run_config)
@@ -151,24 +143,33 @@ def n2v_hypersearch(instance_dir,
 # Running the experiment                            #
 #####################################################
 
+#lista = [20, 30, 40, 50, 60, 70, 80, 90, 100]
+#for i in lista:
 num_vars = 20
 data_path = 'data/rand'
+raytune_dir="hypersearch"
 
 n2v_dim=128
 n2v_num_epochs=30
-raytune_trials=35
-grace_period=5
-scheduler_max_t=25
-raytune_dir="hypersearch"
-exp_name='node2vec'
+n2v_raytune_trials=35
+n2v_grace_period=5
+n2v_scheduler_max_t=25
+n2v_resources_per_trail={"cpu": 6, "gpu": 0.5}
+n2v_exp_name='node2vec'
+n2v_dir = 'n2v_emb'
 
-n2v_dir = 'n2v_emb' 
-#output_dir = 'outputs'
-#log_dir = 'logs'
+pg_batch_size=32
+pg_raytune_trials=35
+pg_grace_period=5
+pg_scheduler_max_t=25
+pg_resources_per_trail={"cpu": 6, "gpu": 0.5}
+pg_exp_name='pg_solver'
+
+output_dir = 'outputs'
+log_dir = 'logs'
 
 paths = paths_for_instances(num_vars, data_path)
-#paths = ['/home/omargp/Documents/Code/Learning-SAT-Solvers/data/rand/0020/0060/rand_n=0020_k=03_m=0060_i=01.cnf',
-#         '/home/omargp/Documents/Code/Learning-SAT-Solvers/data/rand/0020/0060/rand_n=0020_k=03_m=0060_i=02.cnf']
+#paths = ['/home/omargp/Documents/Code/Learning-SAT-Solvers/data/rand/0020/0060/rand_n=0020_k=03_m=0060_i=01.cnf']
 
 
 #####################################################
@@ -179,16 +180,18 @@ paths = paths_for_instances(num_vars, data_path)
 for i, instance_dir in enumerate(paths):
     tail = os.path.split(instance_dir)[1]
     instance_filename = os.path.splitext(tail)[0]
-    exp_path = os.path.join(exp_name, str(n2v_dim), instance_filename)
+    exp_path = os.path.join(n2v_exp_name, str(n2v_dim), instance_filename)
 
+    torch.cuda.empty_cache()
     n2v_hypersearch(instance_dir,
                     n2v_dim=n2v_dim,
                     n2v_num_epochs=n2v_num_epochs,
                     exp_name=exp_path,
-                    raytune_trials=raytune_trials,
+                    raytune_trials=n2v_raytune_trials,
                     raytune_dir=raytune_dir,
-                    grace_period=grace_period,
-                    scheduler_max_t=scheduler_max_t)
+                    grace_period=n2v_grace_period,
+                    scheduler_max_t=n2v_scheduler_max_t,
+                    resources_per_trail=n2v_resources_per_trail)
         
     
 #####################################################
@@ -199,10 +202,11 @@ for i, instance_dir in enumerate(paths):
 node2vec_dir = os.path.join(n2v_dir, str(n2v_dim))
 os.makedirs(node2vec_dir, exist_ok=True)
 
+
 for i, instance_dir in enumerate(paths):
     tail = os.path.split(instance_dir)[1]
     instance_filename = os.path.splitext(tail)[0]
-    exp_path = os.path.join(raytune_dir, exp_name, str(n2v_dim), instance_filename)
+    exp_path = os.path.join(raytune_dir, n2v_exp_name, str(n2v_dim), instance_filename)
     
     # Load best config for this instance
     print(f"\nLoading results from {exp_path} ...")
@@ -214,8 +218,19 @@ for i, instance_dir in enumerate(paths):
     best_config['n2v_verbose'] = 1
     best_config['n2v_raytune'] = False
     best_config['n2v_dir'] = node2vec_dir
-    best_config['n2v_filename'] = f'{instance_filename}.pt'
+    best_config['n2v_filename'] = f'{instance_filename}'
 
+    torch.cuda.empty_cache()
     node2vec_tune(best_config)
 
 
+#####################################################
+# Third step:                                       #
+# Run hyperparameters search for pg_solver          #
+#####################################################
+
+    
+#####################################################
+# Fourth step:                                       #
+# Run pg_solver with the best hyperparameters       #
+#####################################################
